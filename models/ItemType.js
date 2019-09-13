5b21a03d97aac8aa57d7e25b636ce01a43915d9a
Brandt Strom <brandtkstrom@gmail.com>
2019-09-12 21:53:32 -0700

    fix model associations

--- models/ItemType.js
+++ models/ItemType.js
@@ -1,14 +1,36 @@
 module.exports = function(sequelize, DataTypes) {
-    var ItemType = sequelize.define('ItemType', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
+    var ItemType = sequelize.define(
+        'ItemType',
+        {
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            category: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            properties: {
+                type: DataTypes.JSON,
+                allowNull: false
+            }
         },
-        properties: {
-            type: DataTypes.JSON,
-            allowNull: false
+        {
+            indexes: [
+                {
+                    unique: true,
+                    name: 'NAME_CATEGORY',
+                    fields: ['name', 'category']
+                }
+            ]
         }
-    });
+    );
+
+    ItemType.associate = models => {
+        ItemType.hasOne(models.Item, {
+            as: 'Type'
+        });
+    };
 
     return ItemType;
 };
