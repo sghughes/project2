7233a34309c041becbde30f6d008be6a9b380a2b
Brandt Strom <brandtkstrom@gmail.com>
2019-09-14 12:40:43 -0700

    add initial api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,57 @@
-var db = require("../models");
+var db = require('../models');
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(ogs_db) {
-      res.json(ogs_db);
+    // GET routes
+    app.get('/api/listings/types', function(req, res) {
+        db.ItemType.findAll().then(function(data) {
+            res.json(data);
+        });
     });
-  });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(ogs_db) {
-      res.json(ogs_db);
+    app.get('/api/listings', function(req, res) {
+        db.Listing.findAll().then(function(data) {
+            res.json(data);
+        });
     });
-  });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(ogs_db) {
-      res.json(ogs_db);
+    // POST routes
+    app.post('/api/listings', function(req, res) {
+        if (!req.body) {
+            res.status(500).send('Listing data missing.');
+            return;
+        }
+
+        const data = req.body;
+
+        db.Listing.create({
+            title: data.title,
+            description: data.description,
+            image: data.image,
+            item: data.item,
+            condition: data.condition,
+            properties: data.properties,
+            price: data.price,
+            isFree: data.isFree || parseFloat(data.price) == 0,
+            contactZip: data.contactZip,
+            contactEmail: data.contactEmail,
+            contactPhone: data.contactPHone,
+            ItemTypeName: 'clothing'
+        })
+            .then(listing => {
+                res.status(200).json({
+                    message: `Listing created for ${listing.item}`,
+                    data: listing
+                });
+            })
+            .catch(err => res.status(500).json(err));
     });
-  });
+
+    // Delete an example by id
+    app.delete('/api/examples/:id', function(req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function(
+            ogs_db
+        ) {
+            res.json(ogs_db);
+        });
+    });
 };
