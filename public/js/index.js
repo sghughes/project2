64c490e4e6d4af8ee5875ba293ec73af2994008b
Brandt Strom <brandtkstrom@gmail.com>
2019-09-17 01:49:38 -0700

    stringify posted item props json

--- public/js/index.js
+++ public/js/index.js
@@ -3,27 +3,29 @@ $(document).ready(function() {
   // Adding an event listener for when the form is submitted
   $(".newItem").on("submit", function handleFormSubmit(event) {
     event.preventDefault();
+
+   var itemProps = {
+      size: $('#newItemSize').val().toLowerCase(),
+      category: $("#newItemClothingType").val().trim().toLowerCase(),
+      color: $('#newItemColor').val().toLowerCase(),
+      gender: $('#newItemGender').val().toLowerCase()
+   };
+
     // Constructing a newPost object to hand to the database
     var newPost = {
       title: $("#newItemTitle").val().trim(),
       description: $("#newItemDescription").val().trim(),
       image: $('#newItemImageUrl').val().trim(),
-      item: $("#newItemClothingType").val().trim(),
       item_quality: $("#newItemCondition").val(),
-      properties: {
-        size: $('#newItemSize').val(),
-        category: 'clothing',
-        color: $('#newItemColor').val(),
-        gender: $('#newItemGender').val()
-      },
+      properties: JSON.stringify(itemProps),
       price: $("#newItemPrice").val().trim(),
-      isFree: 0,
-      contactZip: $("#newItemZipCode").val().trim(),
+      isFree: false,
+      contactZip: $("#newItemZipCode").val(),
       contactEmail: $("#email").val().trim(),
       contactPhone: $("#phone").val().trim(),
     };
     console.log(newPost);
-    submitPost(newPost);    
+    submitPost(newPost);
   // Submits a new post and brings user to blog page upon completion
   function submitPost(Listing) {
     $.post("/api/listings/", Listing, function() {
