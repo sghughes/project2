9e3064e9ede5cc442eb5515bfdf5ddbeb4133d1d
Stephanie Hughes <stephaniegracehughes@gmail.com>
2019-09-17 07:27:25 -0700

    Added form entry validation. Seller ID is generated on posting submission

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,27 @@
 
 $(document).ready(function() {
+
   // Adding an event listener for when the form is submitted
   $(".newItem").on("submit", function handleFormSubmit(event) {
     event.preventDefault();
+
+    function makeid(length) {
+      var result = '';
+      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      var charactersLength = characters.length;
+      for ( var i = 0; i < length; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+   }
+   var sellerID = makeid(5);
+   console.log(sellerID);
+
     // Constructing a newPost object to hand to the database
     var newPost = {
       title: $("#newItemTitle").val().trim(),
       description: $("#newItemDescription").val().trim(),
       image: $('#newItemImageUrl').val().trim(),
-      item: $("#newItemClothingType").val().trim(),
       item_quality: $("#newItemCondition").val(),
       properties: {
         size: $('#newItemSize').val(),
@@ -17,11 +30,13 @@ $(document).ready(function() {
         gender: $('#newItemGender').val()
       },
       price: $("#newItemPrice").val().trim(),
+      active: 1,
       isFree: 0,
       contactZip: $("#newItemZipCode").val().trim(),
       contactEmail: $("#email").val().trim(),
       contactPhone: $("#phone").val().trim(),
     };
+    alert('Thank you for posting an item. Your seller ID is ' + sellerID + '. Please save this ID as it will be required to remove your posting.')
     console.log(newPost);
     submitPost(newPost);    
   // Submits a new post and brings user to blog page upon completion
@@ -51,4 +66,27 @@ $(document).ready(function() {
       alert('Thanks. Your post has been removed.')
     })
   }
+
+    // phone number formatting
+    $("#phone").keypress(function (e) {
+      if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        return false;
+      }
+      var curchr = this.value.length;
+      var curval = $(this).val();
+      if (curchr == 3 && curval.indexOf("(") <= -1) {
+        $(this).val("(" + curval + ")" + " ");
+      } else if (curchr == 4 && curval.indexOf("(") > -1) {
+        $(this).val(curval + ")-");
+      } else if (curchr == 5 && curval.indexOf(")") > -1) {
+        $(this).val(curval + "-");
+      } else if (curchr == 9) {
+        $(this).val(curval + "-");
+        $(this).attr('maxlength', '14');
+      }
+    });
+
+
+
+
 });
