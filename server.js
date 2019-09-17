32da98ba64aa1b00cde1342ab351ede727c06984
Brandt Strom <brandtkstrom@gmail.com>
2019-09-17 16:48:08 -0700

    fix api route load order

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.set('view engine', 'handlebars');
 
 // Routes
 require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
 require('./routes/locationRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 var syncOptions = { force: false };
 
