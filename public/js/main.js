e171917df31fb75a570b0b219c295298fdcbfe47
Brandt Strom <brandtkstrom@gmail.com>
2019-09-17 14:21:04 -0700

    save geolocation in sessionStorage

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,46 @@
+// Get the current location of the user.
+function getUserLocation() {
+    let location = {};
+
+    // Determine location of user through geolocation (if supported).
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            location = {
+                accuracy: position.coords.accuracy,
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            };
+            sessionStorage.setItem('position', JSON.stringify(location));
+        });
+    } else {
+        // Cannot geolocation user, determine location by zip code
+        // TODO prompt for zip
+        const zip = promptForZip(); // TODO - Get from user input (modal)
+        location = getPositionByZip(zip);
+        sessionStorage.setItem('position', JSON.stringify(location));
+    }
+}
+
+// Look up geolocation by zip code
+function getPositionByZip(zipcode) {
+    // TODO - replace temp test data
+    return {
+        accuracy: 10,
+        latitude: 47.6694141,
+        longitude: -122.1238767
+    }
+}
+
+// Open a prompt to get user zip code
+function promptForZip() {
+    // TODO - replace temp test data
+    return 98052;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    // Determine location of user
+    getUserLocation();
+
     // Search button click event handler
     const searchButton = document.querySelector('#btn-search');
     searchButton.addEventListener('click', async evt => {
@@ -16,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let searchParams = `?item=${searchItem}`;
         if (typeof Filters !== 'undefined') {
             Object.entries(Filters).forEach(([key, value]) => {
-                searchParams += `&${key}=${value}`
+                searchParams += `&${key}=${value}`;
             });
         }
 
@@ -26,11 +68,10 @@ document.addEventListener('DOMContentLoaded', () => {
             const data = await response.json();
 
             // Fire new ListingSearch event for Listeners to handle
-            const searchEvent = new CustomEvent("ListingSearch", {
+            const searchEvent = new CustomEvent('ListingSearch', {
                 detail: data
             });
             this.dispatchEvent(searchEvent);
-
         } catch (err) {
             console.log('ERROR searching for listings', err);
         }
