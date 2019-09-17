e3bfb51ff3cf6b2ce27b60957f18c2efd0431af8
Brandt Strom <brandtkstrom@gmail.com>
2019-09-17 16:58:41 -0700

    add route to get all locations

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,6 +1,14 @@
 var db = require('../models');
 
 module.exports = function(app) {
+    // Look up all stored locations
+    app.get('/api/locations', (req, res) => {
+        db.Location.findAll().then(data => {
+            res.json(data);
+        });
+    })
+
+    // Look up one location
     app.get('/api/locations/:zipcode', (req, res) => {
         // Check DB for location info for requested zip
         const zipcode = req.params.zipcode;
