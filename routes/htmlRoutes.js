f03df8aebb9c88bc13ca337c4a5afcc78efef978
Brandt Strom <brandtkstrom@gmail.com>
2019-09-18 04:04:47 -0700

    remove example code in htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,49 +3,20 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.ItemType.findAll({}).then(function(ogs_db) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: ogs_db
-      });
-    });
+      res.render("index")
   });
 
   app.get("/form", function(req, res) {
-    db.ItemType.findAll({}).then(function(ogs_db) {
-      res.render("form", {
-        msg: "Welcome!",
-        examples: ogs_db
-      });
-    });
+    res.render("form");
   });
 
   app.get("/manage", function(req, res) {
-    db.ItemType.findAll({}).then(function(ogs_db) {
-      res.render("manage", {
-        msg: "Welcome!",
-        examples: ogs_db
-      });
-    });
+    res.render("manage");
   });
 
 
   app.get("/listings", function(req, res) {
-    db.ItemType.findAll({}).then(function(ogs_db) {
-      res.render("listings", {
-        msg: "Welcome!",
-        examples: ogs_db
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(ogs_db) {
-      res.render("example", {
-        example: ogs_db
-      });
-    });
+    res.render("listings");
   });
 
   // Render 404 page for any unmatched routes
