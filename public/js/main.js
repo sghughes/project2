743839a20c79bb1a13bf8eb1ad91635ed381de13
Brandt Strom <brandtkstrom@gmail.com>
2019-09-18 00:05:54 -0700

    prompt & validate for zipcode if not geolocated

--- public/js/main.js
+++ public/js/main.js
@@ -4,37 +4,85 @@ function getUserLocation() {
 
     // Determine location of user through geolocation (if supported).
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            location = {
-                accuracy: position.coords.accuracy,
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-            };
-            sessionStorage.setItem('position', JSON.stringify(location));
-        });
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                location = {
+                    accuracy: position.coords.accuracy,
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                };
+                sessionStorage.setItem('location', JSON.stringify(location));
+            },
+            error => {
+                if (
+                    error.code === error.PERMISSION_DENIED ||
+                    error.code === error.POSITION_UNAVAILABLE
+                ) {
+                    // Could not get location, or denied permission -> ask for zip code
+                    promptForZip();
+                }
+            }
+        );
     } else {
-        // Cannot geolocation user, determine location by zip code
-        // TODO prompt for zip
-        const zip = promptForZip(); // TODO - Get from user input (modal)
-        location = getPositionByZip(zip);
-        sessionStorage.setItem('position', JSON.stringify(location));
-    }
-}
-
-// Look up geolocation by zip code
-function getPositionByZip(zipcode) {
-    // TODO - replace temp test data
-    return {
-        accuracy: 10,
-        latitude: 47.6694141,
-        longitude: -122.1238767
+        // Cannot geolocate user, determine location by zip code
+        promptForZip();
     }
 }
 
 // Open a prompt to get user zip code
 function promptForZip() {
-    // TODO - replace temp test data
-    return 98052;
+    // Prompt for valid zip code until given
+    alertify
+        .prompt()
+        .setting({
+            title: 'Where are you located?',
+            message:
+                'Please enter your zip code to search for nearby listings.',
+            closable: false,
+            closableByDimmer: false,
+            movable: false,
+            labels: { ok: 'Submit' },
+            type: 'text',
+            keepOpen: false,
+            onok: async function(evt, value) {
+                const input = parseInt(value.trim());
+                if (!input) {
+                    this.keepOpen = true;
+                    alertify.error('Invalid zip code');
+                    return;
+                }
+
+                // Potentially valid zip, verify with api request.
+                try {
+                    const response = await fetch(`/api/locations/${input}`);
+                    const location = await response.json();
+                    // Update session storage
+                    sessionStorage.setItem(
+                        'location',
+                        JSON.stringify(location)
+                    );
+
+                    // Display success
+                    this.keepOpen = false;
+                    alertify.success('Thank you!');
+                } catch (err) {
+                    alertify.error(
+                        err.message || 'Could not validate zip code.'
+                    );
+                }
+            },
+            oncancel: function() {
+                this.keepOpen = true;
+                alertify.error('Zip code required');
+            },
+            onclosing: function() {
+                if (this.keepOpen === true) {
+                    return false;
+                }
+                return true;
+            }
+        })
+        .show();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
