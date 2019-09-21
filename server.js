7eb93ffbc597e2bfaba1c3a47b303856822e9119
Brandt Strom <brandtkstrom@gmail.com>
2019-09-21 00:18:14 -0700

    remove morgan

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 var express = require('express');
 var exphbs = require('express-handlebars');
-var morgan = require('morgan');
 var path = require('path');
 
 var db = require('./models');
@@ -13,8 +12,7 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'public')));
-app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Handlebars
 app.engine(
